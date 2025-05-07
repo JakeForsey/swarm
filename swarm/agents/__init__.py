1f9ab9dbab02a6081e04c498e27984a0dbd67559
Jake Forsey <jakeforsey@googlemail.com>
2025-05-07 20:05:47 +0100

    Fix bug in load_agents

--- swarm/agents/__init__.py
+++ swarm/agents/__init__.py
@@ -4,7 +4,10 @@ import pkgutil
 agents_module = importlib.import_module("swarm.agents")
 
 def load_agents():
-    return [importlib.import_module(f"swarm.agents.{name}") for name in pkgutil.iter_modules(agents_module.__path__)]
+    return [
+        importlib.import_module(f"swarm.agents.{info.name}")
+        for info in pkgutil.iter_modules(agents_module.__path__)
+    ]
 
 def get_agent(name):
     return importlib.import_module(f"swarm.agents.{name}")
