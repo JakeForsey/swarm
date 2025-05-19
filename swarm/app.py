0f4727bb05233976846702aea777efc4e5de3273
Jake Forsey <jakeforsey@googlemail.com>
2025-05-19 19:59:42 +0100

    Add API that mimics what the vibevolve algorithm experiences

---
+++ swarm/app.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+from swarm.agents import get_agent
+from swarm.vibevolve import (
+    PROMPT,
+    OPPONENTS,
+    run_tournament,
+)
+
+# OPPONENTS = [
+#     get_agent("random"),
+#     get_agent("chaser"),
+#     get_agent("simple"),
+# ]
+
+templates = Jinja2Templates(directory="templates")
+app = FastAPI()
+
+class CompletionRequest(BaseModel):
+    completion: str
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={
+            "prompt": PROMPT,
+            "completion": "",
+            "results": "",
+        }
+    )
+
+@app.post("/submit")
+async def submit(completion_request: CompletionRequest, request: Request):
+    completion = completion_request.completion
+    results = run_tournament(
+        completion,
+        OPPONENTS,
+        num_rounds_per_matchup=16,
+        episode_length=128,
+    )
+    for result in results:
+        if result["name"] == "tmp_agent":
+            result["name"] = "aggregate"
+        else:
+            result["reward"] = -result["reward"]
+    results.sort(
+        key=lambda x: -x["reward"] if x["name"] != "aggregate" else -2.0
+    )
+    formatted_results = "\n".join([
+        f"{result['name']:>15} {result['reward']:.2f}"
+        for result in results
+    ])
+    return {"results": formatted_results}
