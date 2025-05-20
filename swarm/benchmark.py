9e6f9ff84bfba841feed7a11951b72902a8e26bc
Jake Forsey <jakeforsey@googlemail.com>
2025-05-21 00:51:02 +0100

    Improve benchmarking and add results to the README

--- swarm/benchmark.py
+++ swarm/benchmark.py
@@ -8,33 +8,33 @@ from swarm.agents import get_agent
 
 def run():
     device = jax.default_backend()
-    num_rounds_per_matchup = 1024 * 8
-    episode_length = 64
 
+    episode_length = 128
     agents = [get_agent("random")]
     opponents = [get_agent("random")]
 
-    # Warmup run to pre-jit
-    tournament.run(
-        agents,
-        opponents,
-        num_rounds_per_matchup=1,
-        episode_length=1,
-    )
+    for i in range(20):
+        num_rounds_per_matchup = 2**i
+        # Warmup run to pre-jit
+        tournament.run(
+            agents,
+            opponents,
+            num_rounds_per_matchup=1,
+            episode_length=1,
+        )
 
-    start = time.perf_counter()
-    tournament.run(
-        agents,
-        opponents,
-        num_rounds_per_matchup=num_rounds_per_matchup,
-        episode_length=episode_length,
-    )
-    seconds = time.perf_counter() - start
+        start = time.perf_counter()
+        tournament.run(
+            agents,
+            opponents,
+            num_rounds_per_matchup=num_rounds_per_matchup,
+            episode_length=episode_length,
+        )
+        seconds = time.perf_counter() - start
 
-    batch_size = len(agents) * len(opponents) * 2 * num_rounds_per_matchup
-    steps = batch_size * episode_length
-    steps_per_second = steps / seconds
-
-    print(
-        f"{device=} | {batch_size=} | {steps=:,} | {seconds=:.3f} | {steps_per_second=:,.3f}"
-    )
+        batch_size = len(agents) * len(opponents) * 2 * num_rounds_per_matchup
+        steps = batch_size * episode_length
+        steps_per_second = round(steps / seconds)
+        print(
+            f"{device=} | {batch_size=:>10,} | {steps=:>12,} | {seconds=:>6.3f} | {steps_per_second=:>11,}"
+        )
